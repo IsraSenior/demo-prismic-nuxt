0053bab4e8740fc4750ffbd0c14daadb42700916
Israel Senior <israsenior.dev@gmail.com>
2025-01-24 17:04:35 -0500

    Init commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <pre>
+      {{ home }}
+    </pre>
+    <slice-zone wrapper="main" :components="components" :slices="home.data.slices" />
+  </div>
+</template>
+
+<script setup>
+ import { components } from "~/slices";
+const { client } = usePrismic();
+
+const { data: home } = await useAsyncData("home", () => {
+  return client.getSingle("home", { lang: "en-us" })
+});
+</script>
